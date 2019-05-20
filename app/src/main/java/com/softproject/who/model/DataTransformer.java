b0153abea2a5205104e74d487d73babe2405f917
BogdanGrebenuk <bogdan020699@gmail.com>
2019-05-20 15:24:11 +0300

    all webs

--- app/src/main/java/com/softproject/who/model/DataTransformer.java
+++ app/src/main/java/com/softproject/who/model/DataTransformer.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import com.softproject.who.main.MainPresenter;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -66,6 +67,50 @@ public class DataTransformer {
 
         return userdata;
     }
+
+    public Userdata vkTransform(JSONObject vkObject) throws Exception {
+        JSONArray jsonArray = vkObject.getJSONArray("response");
+        JSONObject jsonObject = jsonArray.getJSONObject(0);
+        Userdata userdata = new Userdata(APIUtils.VK_ID);
+        Iterator<String> keys = jsonObject.keys();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            try {
+                Object value = jsonObject.get(key);
+                switch (key){
+                    case "first_name":
+                        userdata.name = String.valueOf(value);
+                        break;
+                    case "photo_100":
+                        userdata.photo = String.valueOf(value);
+                        break;
+                    case "home_town":
+                        userdata.location = String.valueOf(value);
+                        break;
+                    case "id":
+                        userdata.socialId = String.valueOf(value);
+                        break;
+                    case "sex":
+                        String gender = String.valueOf(value);
+                        if(gender.equals("1")){
+                            userdata.gender = "female";
+                        }else if(gender.equals("2")){
+                            userdata.gender = "male";
+                        }
+                        break;
+                    case "bdate":
+                        userdata.birthday = String.valueOf(value);
+                        break;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return userdata;
+    }
+
+
 
     public Userdata instagramTransform(InstagramUserdata instagramUserdata){
         Userdata userdata = new Userdata(APIUtils.INSTAGRAM_ID);
